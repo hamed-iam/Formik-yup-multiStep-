a630ed6b6220ebdb965ac9e369f777c63ac3f7f1
Hamed Ghazali <hamedg20@yahoo.com>
2021-07-16 18:33:12 +0430

    first

---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CircularProgress,
+  Grid,
+  Step,
+  StepLabel,
+  Stepper,
+} from '@material-ui/core';
+import { Field, Form, Formik, FormikConfig, FormikValues } from 'formik';
+import { CheckboxWithLabel, TextField } from 'formik-material-ui';
+import React, { useState } from 'react';
+import { mixed, number, object } from 'yup';
+
+const sleep = (time) => new Promise((acc) => setTimeout(acc, time));
+
+export default function Home() {
+  return (
+    <Card>
+      <CardContent>
+        
+      </CardContent>
+    </Card>
+  );
+}
