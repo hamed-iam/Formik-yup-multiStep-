ff6f5fcecfed71fd83902a48732bfaeca3c41f97
Hamed Ghazali <hamedg20@yahoo.com>
2021-07-16 19:04:10 +0430

    second

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,13 +14,53 @@ import { CheckboxWithLabel, TextField } from 'formik-material-ui';
 import React, { useState } from 'react';
 import { mixed, number, object } from 'yup';
 
-const sleep = (time) => new Promise((acc) => setTimeout(acc, time));
+const initialValues = {
+  first_name: '',
+  last_name: '',
+  millionaire: false,
+  money: 0,
+  description: '',
+};
 
 export default function Home() {
   return (
     <Card>
       <CardContent>
-        
+        <Formik
+          validationSchema={object({
+            money: mixed().when('millionaire', {
+              is: true,
+              then: number().required().min(1_000_000),
+              otherwise: number().required(),
+            }),
+          })}
+          initialValues={initialValues}
+          onSubmit={(data) => {
+            console.log(data);
+          }}
+        >
+          <Form autoComplete="off">
+            <Field name="first_name" component={TextField} label="First Name" />
+            <Field name="last_name" component={TextField} label="Last Name" />
+            <Field
+              name="millionaire"
+              type="checkbox"
+              component={CheckboxWithLabel}
+              Label={{ label: 'I am a millionaire' }}
+            />
+            <Field
+              name="money"
+              type="number"
+              component={TextField}
+              label="All The Money I Have"
+            />
+            <Field
+              name="description"
+              component={TextField}
+              label="Description"
+            />
+          </Form>
+        </Formik>
       </CardContent>
     </Card>
   );
